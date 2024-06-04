29b097fe0e5fb1edf783870bc2de6d4f2d031977
ilia makarov <ilia.makarov@me.com>
2024-06-04 16:11:07 +0300

    Removed iframe-resizer

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { DeskproAppProvider } from "@deskpro/app-sdk";
-import "iframe-resizer/js/iframeResizer.contentWindow.js";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as Element);
